cda923131600cb56dfd82e174e640861f3a22a35
Anwesh43 <anweshthecool0@gmail.com>
2017-12-17 21:31:19 +0530

    adding underground square

---
+++ underground-square.js
@@ -0,0 +1,43 @@
+const h = window.innerHeight*9/10
+class UndergroundSquare {
+    constructor(i) {
+        this.createDom(i)
+    }
+    createDom(i) {
+
+        this.y = h - i*100
+        this.oy = this.y
+        this.div = document.createElement('div')
+        this.div.style.borderTop = '1px solid green'
+        this.div.style.borderBottom = '1px solid red'
+        this.div.style.width = 100
+        this.div.style.position = 'absolute'
+        this.div.style.top = this.y
+        this.div.style.left = w/2 -50
+        this.div.style.height = 0
+        this.div.style.borderRadius = '15px'
+    }
+    update(stopcb) {
+        this.y -= 10
+        this.div.style.top = this.y
+        this.div.style.height = this.oy - this.y
+        if(parseInt(this.div.style.height) >= 100) {
+            stopcb()
+        }
+    }
+}
+class UndergroundSquareContainer {
+    constructor() {
+        this.squares = []
+
+    }
+    update(stopcb) {
+        if(this.curr) {
+            this.curr.update(stopcb)
+        }
+    }
+    create() {
+        this.curr = new UndergroundSquare()
+        this.squares.push(this.curr)
+    }
+}
